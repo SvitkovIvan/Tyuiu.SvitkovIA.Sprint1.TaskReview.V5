a5e67c18820b779d6692add4d7e7c98ff472cac2
Tyuiu.SvitkovIA@outlook.com <svitkovia@mail.ru>
2023-10-04 15:21:41 +0500

    TaskReview.V5

--- Tyuiu.SvitkovIA.Sprint1.TaskReview.V5/Program.cs
+++ Tyuiu.SvitkovIA.Sprint1.TaskReview.V5/Program.cs
@@ -15,10 +15,31 @@ namespace Tyuiu.SvitkovIA.Sprint1.TaskReview.V5
         {
             DataService dataService = new DataService();
 
+            Console.Title = "Спринт #1 | Выполнил: Свитков И. А. | АСОиУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #TaskReview                                                     *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Свитков Иван Андреевич | АСОиУб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            
+       
+
+
             double x;
 
             Console.WriteLine("Введите значение X:");
             x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
 
             Console.WriteLine(dataService.Calculate(x));
 
